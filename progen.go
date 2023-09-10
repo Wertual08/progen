19c6849fc6d6c33b512b306211c62967b790a4a0
wertual <wertual08@gmail.com>
2023-09-10 22:24:45 +0300

    Better recursive file search

--- progen.go
+++ progen.go
@@ -3,25 +3,28 @@ package progen
 import (
 	"errors"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func Run(
-    rootDirectory string,
+    rootPath string,
+    protoDirectory string,
     outputDirectory string,
 ) {
     log.Println("INFO: Cleaning up...")
 
-    err := cleanup(rootDirectory)
+    err := cleanup(rootPath, outputDirectory)
     if err != nil {
         log.Fatalf("FAIL: Can not list generated files\n%s", err)
         os.Exit(1)
     }
     
-    err = generate(rootDirectory, outputDirectory)
+    err = generate(rootPath, protoDirectory, outputDirectory)
     if err != nil {
         log.Fatalf("FAIL: Can not generate proto\n%s", err)
         os.Exit(2)
@@ -30,53 +33,107 @@ func Run(
     log.Println("INFO: Proto generated")
 }
 
-func cleanup(rootDirectory string) error {
-    matches, err := filepath.Glob(
-        fmt.Sprintf("%s/*/*.pb.go", rootDirectory),
+func cleanup(
+    rootPath string,
+    outputDirectory string,
+) error {
+    err := filepath.WalkDir(
+        filepath.Join(rootPath, outputDirectory),
+        func(path string, d fs.DirEntry, err error) error {
+            if err != nil {
+                return err
+            }
+            if !strings.HasSuffix(path, ".pb.go") {
+                return nil
+            }
+
+            return os.Remove(path)
+        },
     )
     if err != nil {
         return err
     }
 
-    for _, match := range matches {
-        if err := os.Remove(match); err != nil {
-            return err
-        }
-    }
 
     return nil
 }
 
-func generate(rootDirectory string, outputDirectory string) error {
-    matches, err := filepath.Glob(
-        fmt.Sprintf("%s/*/*.proto", rootDirectory),
+func generate(
+    rootPath string, 
+    protoDirectory string,
+    outputDirectory string,
+) error {
+    targetProtoDirectory := filepath.Join(rootPath, protoDirectory)
+
+    argProtoPath := fmt.Sprintf(
+        "--proto_path=%s/%s", 
+        rootPath, 
+        protoDirectory,
+    )
+    argGoOut := fmt.Sprintf(
+        "--go_out=%s", 
+        rootPath,
+    )
+    argGoGrpcOut := fmt.Sprintf(
+        "--go-grpc_out=%s", 
+        rootPath,
+    )
+
+    err := filepath.WalkDir(
+        targetProtoDirectory,
+        func(path string, d fs.DirEntry, err error) error {
+            if err != nil {
+                return err
+            }
+            if !strings.HasSuffix(path, ".proto") {
+                return nil
+            }
+
+            log.Printf("INFO: Generating %s...\n", path)
+
+            relativePath := strings.TrimPrefix(path, targetProtoDirectory)[1:]
+
+            fileModule := strings.TrimSuffix(
+                relativePath,
+                ".proto",
+            )
+                
+            argGoOpt := fmt.Sprintf(
+                "--go_opt=M%s=%s/%s", 
+                relativePath, 
+                outputDirectory, 
+                fileModule,
+            )
+            argGoGrpcOpt := fmt.Sprintf(
+                "--go-grpc_opt=M%s=%s/%s", 
+                relativePath, 
+                outputDirectory, 
+                fileModule,
+            )
+
+            cmd := exec.Command(
+                "protoc", 
+                argProtoPath,
+                argGoOut,
+                argGoGrpcOut,
+                argGoOpt,
+                argGoGrpcOpt,
+                path,
+            )
+
+            if _, err := cmd.Output(); err != nil {
+                if err, ok := err.(*exec.ExitError); ok {
+                    return errors.Join(err, errors.New(string(err.Stderr)))
+                }
+
+                return err
+            }
+
+            return nil
+        },
     )
     if err != nil {
         return err
-    }
-
-    protoPath := fmt.Sprintf("--proto_path=%s/proto", rootDirectory)
-    goOut := fmt.Sprintf("--go_out=%s", outputDirectory)
-    goGrpcOut := fmt.Sprintf("--go-grpc_out=%s", outputDirectory)
-    
-    for _, match := range matches {
-        log.Printf("INFO: Generating %s...\n", match)
-
-        cmd := exec.Command(
-            "protoc", 
-            protoPath,
-            match,
-            goOut,
-            goGrpcOut,
-        )
-
-        if _, err := cmd.Output(); err != nil {
-            if err, ok := err.(*exec.ExitError); ok {
-                return errors.Join(err, errors.New(string(err.Stderr)))
-            }
-
-            return err
-        }
     }
 
     return nil
